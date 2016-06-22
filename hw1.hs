8b51b11379ca3c64fda05007fc46b021c683de6d
Chase Meadors <c.ed.mead@gmail.com>
2016-06-22 18:57:51 -0500

    Tabs to spaces in hw1.hs

--- hw1.hs
+++ hw1.hs
@@ -1,23 +1,23 @@
-toDigitsRev	:: Integer -> [Integer]
+toDigitsRev    :: Integer -> [Integer]
 toDigitsRev n
-	| n < 1 	= []
-	| n < 10 	= [n]
-	| otherwise = (mod n 10) : (toDigitsRev (div n 10))
+    | n < 1     = []
+    | n < 10    = [n]
+    | otherwise = (mod n 10) : (toDigitsRev (div n 10))
 
 toDigits :: Integer -> [Integer]
 toDigits = reverse . toDigitsRev
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther [] 		= []
-doubleEveryOther [x] 		= [x]
+doubleEveryOther []         = []
+doubleEveryOther [x]        = [x]
 doubleEveryOther (x:(y:zs)) = [x, 2*y] ++ (doubleEveryOther zs)
 
 doubleEveryOtherRight :: [Integer] -> [Integer]
 doubleEveryOtherRight = reverse . doubleEveryOther . reverse
 
 allToDigits :: [Integer] -> [Integer]
-allToDigits [] 		= []
-allToDigits (x:ys) 	= (toDigits x) ++ (allToDigits ys)
+allToDigits []     = []
+allToDigits (x:ys) = (toDigits x) ++ (allToDigits ys)
 
 validate :: Integer -> Bool
 validate n = ((sum . allToDigits . doubleEveryOtherRight . toDigits) n) `mod` 10 == 0
@@ -28,5 +28,5 @@ type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi n p1 p2 p3
-	| n < 1 	= []
-	| otherwise = (hanoi (n-1) p1 p3 p2) ++ [(p1, p2)] ++ (hanoi (n-1) p3 p2 p1)
+    | n < 1     = []
+    | otherwise = (hanoi (n-1) p1 p3 p2) ++ [(p1, p2)] ++ (hanoi (n-1) p3 p2 p1)
