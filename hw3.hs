0c4ba6f1bf65b163d4b516871df9c88e556beb4f
Chase Meadors <c.ed.mead@gmail.com>
2016-06-22 21:16:22 -0500

    work

---
+++ hw3.hs
@@ -0,0 +1,59 @@
+module Golf where
+import Data.List
+
+-- Task 1 --
+
+chunk :: Int -> [a] -> [[a]]
+chunk _ []   = []
+chunk n list = [take n list] ++ (chunk n (drop n list))
+
+everyNth :: Int -> [a] -> [a]
+everyNth n list  = map last (filter (\x -> (length x) >= n) (chunk n list))
+
+
+-- Which skips is the best style?
+
+skips :: [a] -> [[a]]
+skips list = [everyNth n list | n <- [1..(length list)]]
+
+skips' :: [a] -> [[a]]
+skips' list = map ((flip everyNth) list) [1..(length list)]
+
+skips'' :: [a] -> [[a]]
+skips'' list = reverse (skipsInner (length list) list)
+    where
+        skipsInner 0 _    = []
+        skipsInner n list = [everyNth n list] ++ (skipsInner (n-1) list)
+
+
+-- Task 2 --
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima (x:y:z:ws) = (if (x < y && z < y) then [y] else []) ++ (localMaxima (y:z:ws))
+localMaxima other      = []
+
+
+-- Task 3 --
+
+dropOne :: [a] -> [a]
+dropOne []     = []
+dropOne (x:xs) = xs
+
+popAll :: [[a]] -> [[a]]
+popAll = map dropOne
+
+toRuns :: [Integer] -> [[Integer]]
+toRuns list = fillIn (group (sort list))
+    where
+        fillIn list = map (existingOrEmpty list) [0..9]
+        existingOrEmpty list n = if (length result) > 0 then (head result) else []
+            where result = filter (\x -> (head x) == n) list
+
+histogram :: [Integer] -> String
+histogram list = histogramInner (toRuns list) ++ "\n==========\n0123456789\n"
+    where
+        histogramInner list
+            | noneLeft    = ""
+            | otherwise   = (histogramInner (popAll list)) ++ "\n" ++ (foldl (++) "" (map starIfNonEmpty list))
+            where noneLeft = all (\x -> (length x) == 0) list
+                  starIfNonEmpty list = if (length list) > 0 then "*" else " "
